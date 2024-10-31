d1260b8f3aa8e5be2f055c961d3b0474d3f636f9
KrisNavela <krisnavela@gmail.com>
2024-10-31 11:40:47 +0800

    optimize: view index in dashboard 3

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -84,7 +84,7 @@
 
     <div class="py-1" >
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex space-x-4">
                 <div class="p-6 text-gray-900">
                     <div class="flex justify-end">
                         <a href="{{ route('requisitions.create') }}" class="bg-blue-500 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Create</a>
@@ -92,7 +92,7 @@
                     
                     
                     
-                    <table class="flex space-x-4 min-w-full divide-y divide-gray-200 mt-2">
+                    <table class="min-w-full divide-y divide-gray-200 mt-2">
                         <thead class="bg-gray-50">
                             <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">ID</th>
                             <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Requestion Number</th>
