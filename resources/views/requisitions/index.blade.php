27185cba86f1696f9eed8c3385d729e2ab90aa2a
KrisNavela <krisnavela@gmail.com>
2024-11-04 14:27:32 +0800

    Dashboard View 42

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -84,7 +84,7 @@
 
     <!-- Card view for mobile screens -->
     <div class="p-6 text-gray-900 overflow-x-auto" >
-        <div class="space-y-4">
+        <div class="space-y-6">
 
             <div class="flex justify-end">
                 <a href="{{ route('requisitions.create') }}" class="bg-blue-500 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Create</a>
