4afc976c8062b9aa19c6e417092b05c51aef477e
KrisNavela <krisnavela@gmail.com>
2024-11-04 16:25:51 +0800

    Create requisition 16

--- resources/views/requisitions/create.blade.php
+++ resources/views/requisitions/create.blade.php
@@ -57,7 +57,7 @@
                                 <x-input-error :messages="$errors->get('replenishment_year')" class="mt-2" />
                             </div>
 
-                    <div class="overflow-auto max-h-64">
+                    <div class="container mx-auto p-4">
                         <table class="min-w-full divide-y divide-gray-200 mt-2">
                             <thead class="bg-gray-50">
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Item Name</th>
@@ -65,10 +65,11 @@
                                 <th class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase">Unit</th>
                                 <th class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase"></th>
                             </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                                <template x-for="(item, index) in items" :key="index">
+                            <tbody class="bg-white divide-y divide-gray-200" id="table-body">
+                                
+                                <template id="row-template" x-for="(item, index) in items" :key="index">
                                     <tr class="hover:bg-gray-200">
-                                        <td class="px-2 py-2 text-xs">
+                                        <td class="px-2 py-2">
                                             <select class="" x-model="item.id" :name="'items['+index+'][id]'" >
                                                 <option value="">Please Select Item</option>
                                                 @foreach($items as $item)
@@ -76,28 +77,31 @@
                                                 @endforeach
                                             </select>
                                         </td>
-
-                                        <td class="px-2 py-2 text-xs">
+                                        <td class="px-2 py-2">
                                             <input type="number" style="width: 100px;" x-model="item.quantity" :name="'items['+index+'][quantity]'">
                                         </td>
-
-                                        <td class="px-2 py-2 text-xs">
+                                        <td class="px-2 py-2">
                                             <select class="" x-model="item.quantity_unit" :name="'items['+index+'][quantity_unit]'">
                                                 <option value="Pad">Pad</option>
                                                 <option value="Pcs">Pcs</option>
                                                 <option value="Set">Set</option>
                                             </select>
                                         </td>
-
-                                        <td class="px-2 py-2 text-xs">
+                                        <td class="px-2 py-2">
                                             <button type="button" class="bg-red-500 text-white hover:bg-red-700 text-sm px-2 py-1 rounded-md" @click="removeItem(index)">Remove</button>
                                         </td>
                                     </tr>
                                 </template>
+
+
+
                             </tbody>
                         </table>
                     </div>
-
+                    <script>
+                        const template = document.getElementById('row-template');
+                        const tableBody = document.getElementById('table-body');
+                    </script>
                         <div>
                             <button type="button" @click="addItem" class="bg-blue-500 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">
                                 Add Item
