5596dfdc7b2f15c0c57405f37b0918322899301d
KrisNavela <krisnavela@gmail.com>
2024-11-04 14:38:29 +0800

    Dashboard View 45

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
 <div class="py-7">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
         <div class="bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
                 <!-- Navigation Links -->
@@ -77,8 +77,8 @@
                     @endif
                 </div>
             </div>
+        
         </div>
-    </div>
 </div>
 
 
