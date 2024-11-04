2ec13763e28946fc0e372544589c0076c365b157
KrisNavela <krisnavela@gmail.com>
2024-11-04 08:47:39 +0800

    Dashboard view 3

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -100,7 +100,8 @@
                     </div>
 
                     <div class="overflow-auto round-lg shadow hidden md:block">
-                    <table class="min-w-full bg-white divide-y divide-gray-200 mt-2">
+
+                    <table class="w-full">
                         <thead class="bg-gray-50">
                             <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">ID</th>
                             <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Requestion Number</th>
@@ -142,6 +143,8 @@
                             @endforeach
                         </tbody>
                     </table>
+
+
                     <div>
 
                     <div>
