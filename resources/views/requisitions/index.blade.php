5fde82362c46850fc368949bb34f26bc24651a77
KrisNavela <krisnavela@gmail.com>
2024-11-04 13:36:31 +0800

    Dashboard View 26

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -77,7 +77,7 @@
     </div>
 </div>
 
-
+@extends('layouts.app')
 @section('content')
     <div class="container mx-auto px-4">
         <h1 class="text-2xl font-bold mb-6">Responsive Data View</h1>
