c8a660fcd01601dd7d85d4e951bb66ade1b75321
KrisNavela <krisnavela@gmail.com>
2024-11-04 15:39:18 +0800

    Update Data View

--- resources/views/forbondapprovalrequisitions/index.blade.php
+++ resources/views/forbondapprovalrequisitions/index.blade.php
@@ -79,54 +79,37 @@
     </div>
 </div>
 
-    <div class="py-1">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <table class="min-w-full divide-y divide-gray-200 mt-2">
-                        <thead class="bg-gray-50">
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">ID</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Requestion Number</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Date</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Status</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">User</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase"># Items</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Branch</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Type</th>
-                            <th class="px-6 py-3 text=left text-xs font-medium text-black uppercase">Actions</th>
-                        </thead>                
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($requisitions as $requisition)
-                            <tr class="px-6 py-4 whitespace-nowrap">
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->id }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->req_no }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->req_date }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->status }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->user->first_name }} {{ $requisition->user->last_name }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->items_count }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->user->branch->branch_name }}</td>
-                                <td class="px-6 py-3 text=left text-xs font-medium text-gray-800 uppercase"> {{ $requisition->user->branch->type_office }}</td>
 
-                                <td>
-                                    <a href="{{ route('forbondapprovalrequisitions.show', $requisition->id) }}" class="bg-blue-300 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Show</a>
-                                    <a href="{{ route('forbondapprovalrequisitions.edit', $requisition->id)}}" class="bg-green-500 text-white hover:bg-green-700 text-sm px-2 py-1 rounded-md">Edit</a>
-                                </td>
+    <!-- Card view for mobile screens -->
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" >
+        <div class="space-y-4">
 
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+            <div class="flex justify-end">
+                <a href="{{ route('requisitions.create') }}" class="bg-blue-500 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Create</a>
+            </div>
 
-                    <div>
-                        {{ $requisitions->links() }}
+            <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                @foreach ($requisitions as $requisition)
+                <div class="bg-white shadow-md rounded-lg p-6">
+                    <h2 class="text-lg font-semibold text-gray-900">{{ $requisition->id }}</h2>
+                    <p class="text-sm text-gray-600">Requestion Number: {{ $requisition->req_no }}</p>
+                    <p class="text-sm text-gray-600">Date: {{ $requisition->req_date }}</p>
+                    <p class="text-sm text-gray-600">Status: {{ $requisition->status }}</p>
+                    <p class="text-sm text-gray-600">Request By: {{ $requisition->user->first_name }} {{ $requisition->user->last_name }}</p>
+                    <p class="text-sm text-gray-600"># Items: {{ $requisition->items_count }}</p>
+                    <p class="text-sm text-gray-600">Branch: {{ $requisition->user->branch->branch_name }}</p>
+                    <p class="text-sm text-gray-600">Type: {{ $requisition->user->branch->type_office }}</p>
+                    <div class="mt-4">
+                        <a href="{{ route('forbondapprovalrequisitions.show', $requisition->id) }}" class="bg-blue-300 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Show</a>
+                        <a href="{{ route('forbondapprovalrequisitions.edit', $requisition->id)}}" class="bg-green-500 text-white hover:bg-green-700 text-sm px-2 py-1 rounded-md">Edit</a>
                     </div>
                 </div>
-
-                
+                @endforeach
+            </div>
+            <div>
+                {{ $requisitions->links() }}
             </div>
         </div>
     </div>
-
-
     
 </x-app-layout>
