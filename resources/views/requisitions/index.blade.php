5460009170c77ebd45c30c6cc9f17cf48108fc68
KrisNavela <krisnavela@gmail.com>
2024-11-04 11:57:58 +0800

    Dashboard View 15

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -89,52 +89,69 @@
                     </div>
 
                     <div class="overflow-auto round-lg shadow md:block">
+                        <table class="w-full">
+                            <thead class="bg-gray-50 border-b-2 border-gray-200">
+                                <th class="w-20 p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">ID</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Requestion Number</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Date</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Status</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">User</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase"># Items</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Branch</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Type</th>
+                                <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Actions</th>
+                            </thead>                
+                            <tbody class="divide-y divide-gray-100">
+                                @foreach ($requisitions as $requisition)
+                                <tr class="bg-white">
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->id }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->req_no }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->req_date }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->status }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->user->first_name }} {{ $requisition->user->last_name }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->items_count }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->user->branch->branch_name }}</td>
+                                    <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->user->branch->type_office }}</td>
 
-                    <table class="w-full">
-                        <thead class="bg-gray-50 border-b-2 border-gray-200">
-                            <th class="w-20 p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">ID</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Requestion Number</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Date</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Status</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">User</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase"># Items</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Branch</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Type</th>
-                            <th class="p-3 text-sm font-semibold tracking-wide text-left text-black uppercase">Actions</th>
-                        </thead>                
-                        <tbody class="divide-y divide-gray-100">
-                            @foreach ($requisitions as $requisition)
-                            <tr class="bg-white">
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->id }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->req_no }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->req_date }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->status }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->user->first_name }} {{ $requisition->user->last_name }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->items_count }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->user->branch->branch_name }}</td>
-                                <td class="p-3 text-sm text-gray-700 whitespace-nowrap uppercase"> {{ $requisition->user->branch->type_office }}</td>
+                                    <td>
 
-                                <td>
+                                    <a href="{{ route('requisitions.show', $requisition->id) }}" class="bg-blue-300 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Show</a>
+                                    @can('view', $requisition)
+                                        <a href="{{ route('requisitions.edit', $requisition->id)}}" class="bg-green-500 text-white hover:bg-green-700 text-sm px-2 py-1 rounded-md">Edit</a>
 
-                                <a href="{{ route('requisitions.show', $requisition->id) }}" class="bg-blue-300 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Show</a>
-                                @can('view', $requisition)
-                                    <a href="{{ route('requisitions.edit', $requisition->id)}}" class="bg-green-500 text-white hover:bg-green-700 text-sm px-2 py-1 rounded-md">Edit</a>
-
-                                    <form action="{{ route('requisitions.destroy', $requisition->id) }}" method="POST" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="bg-red-500 text-white hover:bg-red-700 text-sm px-2 py-1 rounded-md">Delete</button> 
-                                    </form>
-                                @endcan
-                            
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-
-
+                                        <form action="{{ route('requisitions.destroy', $requisition->id) }}" method="POST" class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="bg-red-500 text-white hover:bg-red-700 text-sm px-2 py-1 rounded-md">Delete</button> 
+                                        </form>
+                                    @endcan
+                                
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
+
+                    <div class="grid grid-cols-1 gap-4 md:hidden">
+                        <div class="bg-white p-4">
+                            <div>
+                                <div>ID</div>
+                                <div>Requestion Number</div>
+                                <div>Date</div>
+                                <div>Status</div>
+                                <div>User</div>
+                                <div># Items</div>
+                                <div>Branch</div>
+                                <div>Type</div>
+                                <div>Actions</div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
                     <div>
                         {{ $requisitions->links() }}
                     </div>
