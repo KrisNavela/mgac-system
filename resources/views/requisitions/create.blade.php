f610c963d4814586ff7440e05b2cc5f092148e51
KrisNavela <krisnavela@gmail.com>
2024-11-04 15:46:16 +0800

    Create Requisition View in Mobile

--- resources/views/requisitions/create.blade.php
+++ resources/views/requisitions/create.blade.php
@@ -67,7 +67,7 @@
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Unit</th>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"></th>
                             </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
+                            <tbody class="bg-white divide-y divide-gray-200 sm:overflow-x-auto sm:whitespace-nowrap md:overflow-x-visible md:whitespace-normal">
                                 <template x-for="(item, index) in items" :key="index">
                                     <tr class="hover:bg-gray-200">
                                         <td class="px-2 py-2">
