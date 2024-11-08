55d6c0337ce9151875bb8a69fbade98ded2a5601
KrisNavela <krisnavela@gmail.com>
2024-11-08 13:48:19 +0800

    Update: Restore Add message alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        
-        <!-- Alpine.js (CDN) -->
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 
         <!-- Scripts -->
