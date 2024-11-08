92c293d2c7890ac15e3dbecb0fe2884eeec37ba0
KrisNavela <krisnavela@gmail.com>
2024-11-08 13:32:59 +0800

    Update: Add message alert in create requisition 1

--- resources/views/requisitions/create.blade.php
+++ resources/views/requisitions/create.blade.php
@@ -25,7 +25,12 @@
                     this.items.splice(index, 1);
                 }
                 }">
-
+                <head>
+                    <!-- Add SweetAlert2 CSS and JS -->
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+                        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                    </head>
+                    
                     <form action="{{ route('requisitions.store') }}" method="POST">
                         @csrf
 
