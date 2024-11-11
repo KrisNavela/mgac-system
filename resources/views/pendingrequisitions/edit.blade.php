b23343ae0b081e730a73fc8b4b825b408700e44a
KrisNavela <krisnavela@gmail.com>
2024-11-11 16:06:08 +0800

    text center 2

--- resources/views/pendingrequisitions/edit.blade.php
+++ resources/views/pendingrequisitions/edit.blade.php
@@ -24,7 +24,7 @@
         <div class="flex justify-center space-x-4">
             <div class="w-1/2 p-4 bg-gray-200">
                 <p class="text-center">Status</p>
-                <p class="uppercase">{{ $requisition->status }}</p>
+                <p class="uppercase text-center">{{ $requisition->status }}</p>
             </div>
             <div class="w-1/2 p-4 bg-gray-200">
                 <p>Bonds Approval</p>
