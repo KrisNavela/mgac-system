18f89ecf2cdc6d36b6f7596a3c74d3ac13d1680c
KrisNavela <krisnavela@gmail.com>
2024-11-19 13:48:26 +0800

    Update design in flash 4

--- resources/views/requisitions/edit.blade.php
+++ resources/views/requisitions/edit.blade.php
@@ -100,8 +100,9 @@
 
                     <!-- Success Notification -->
                     @if (session('success'))
-                        <div class="mb-4 p-4 bg-green-100 text-green-800 border border-green-300 rounded">
-                            {{ session('success') }}
+                        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                            <strong class="font-bold">Success!</strong>
+                            <span class="block sm:inline">{{ session('success') }}</span>
                         </div>
                     @endif
 
