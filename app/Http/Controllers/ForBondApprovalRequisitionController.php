6b2e1fb8b6f95c56e19089088ad264156bed54c5
KrisNavela <krisnavela@gmail.com>
2024-11-20 13:54:48 +0800

    update bond controller

--- app/Http/Controllers/ForBondApprovalRequisitionController.php
+++ app/Http/Controllers/ForBondApprovalRequisitionController.php
@@ -48,24 +48,19 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'for approval')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $collmngapprovalCount = Requisition::withCount('items')
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'approved')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $cancelrequisitionsCount = Requisition::withCount('items')
             ->where('status', '=', 'Cancelled')
             ->count();
-
-            //$approvedrequisitionsCount = Requisition::whereHas('user', function ($query) {
-            //    $query->whereHas('branch', function ($query1) {
-            //        $query1->where('type_office', 'Agency');}
-            //);})
-            //->where('finalapproval_status', '=', 'for approval')
-            //->count();
 
             $approvedrequisitionsCount = Requisition::withCount('items')
             ->where('finalapproval_status', '=', 'for approval')
@@ -76,10 +71,12 @@ class ForBondApprovalRequisitionController extends Controller
             ->count();
 
             $treasuryapprovalCount = Requisition::withCount('items')
+            ->where('finalapproval_status', '=', 'approved')
             ->where('treasuryapproval_status', '=', 'for approval')
             ->count();
 
             $cocapprovalCount = Requisition::withCount('items')
+            ->where('finalapproval_status', '=', 'approved')
             ->where('cocapproval_status', '=', 'for approval')
             ->count();
 
@@ -146,6 +143,7 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'for approval')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $collmngapprovalCount = Requisition::whereHas('user', function ($query) {
@@ -155,6 +153,7 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'approved')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $cancelrequisitionsCount = Requisition::whereHas('user', function ($query) {
@@ -183,6 +182,7 @@ class ForBondApprovalRequisitionController extends Controller
                     $query1->where('type_office', 'Branch');}
             );})
             ->where('treasuryapproval_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $cocapprovalCount = Requisition::whereHas('user', function ($query) {
@@ -190,6 +190,7 @@ class ForBondApprovalRequisitionController extends Controller
                     $query1->where('type_office', 'Branch');}
             );})
             ->where('cocapproval_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $requisitions = Requisition::whereHas('user', function ($query) {
@@ -259,6 +260,7 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'for approval')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $collmngapprovalCount = Requisition::whereHas('user', function ($query) {
@@ -268,6 +270,7 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'approved')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $cancelrequisitionsCount = Requisition::whereHas('user', function ($query) {
@@ -296,6 +299,7 @@ class ForBondApprovalRequisitionController extends Controller
                     $query1->where('type_office', 'Agency');}
             );})
             ->where('treasuryapproval_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count();
 
             $cocapprovalCount = Requisition::whereHas('user', function ($query) {
@@ -303,6 +307,7 @@ class ForBondApprovalRequisitionController extends Controller
                     $query1->where('type_office', 'Agency');}
             );})
             ->where('cocapproval_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->count(); 
 
             $requisitions = Requisition::whereHas('user', function ($query) {
@@ -362,6 +367,7 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'for approval')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->where('user_id', $userId)
             ->count();
         
@@ -369,6 +375,7 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'approved')
             ->where('collmanager_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->where('user_id', $userId)
             ->count();
 
@@ -381,19 +388,21 @@ class ForBondApprovalRequisitionController extends Controller
             ->where('finalapproval_status', '=', 'for approval')
             ->where('user_id', $userId)
             ->count();
-        
+
         $fortransmittalCount = Requisition::withCount('items')
             ->where('status', '=', 'approved')
             ->where('user_id', $userId)
             ->count();
-        
+
         $treasuryapprovalCount = Requisition::withCount('items')
             ->where('treasuryapproval_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->where('user_id', $userId)
             ->count();
 
         $cocapprovalCount = Requisition::withCount('items')
             ->where('cocapproval_status', '=', 'for approval')
+            ->where('finalapproval_status', '=', 'approved')
             ->where('user_id', $userId)
             ->count();
         
