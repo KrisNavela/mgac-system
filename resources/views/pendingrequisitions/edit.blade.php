1634444eea677df7e6100da2cbc6fb040575c182
KrisNavela <krisnavela@gmail.com>
2024-11-22 09:49:48 +0800

    add PUT in forward to final approval

--- resources/views/pendingrequisitions/edit.blade.php
+++ resources/views/pendingrequisitions/edit.blade.php
@@ -140,7 +140,14 @@
                     </form>
 
                     <div class="py-2">
-                        <a href="{{ route('pendingrequisitions.update.forfinalapproval', $requisition->id) }}" class="bg-blue-300 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Forward to Final Approval</a>
+                        <form method="POST" action="{{ route('pendingrequisitions.update.forfinalapproval', $requisition->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <button class="bg-green-500 text-white hover:bg-green-700 text-sm px-2 py-1 rounded-md">
+                                Forward to Final Approval
+                            </button>
+
+                        </form>
                     </div> 
 
                     
