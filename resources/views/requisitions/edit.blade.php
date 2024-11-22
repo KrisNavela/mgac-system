dc03ff71648b8e4082b6b0d44d9a897124c1a52e
KrisNavela <krisnavela@gmail.com>
2024-11-22 15:14:11 +0800

    update 2

--- resources/views/requisitions/edit.blade.php
+++ resources/views/requisitions/edit.blade.php
@@ -135,8 +135,8 @@
                         <div class="mt-4">
                             <x-input-label for="coc_request_status" :value="__('COC Request')" />
                             <select name="coc_request_status" id="" style="width: 300px;">
-                                <option value="No" {{ 'No' === $requisition->coc_request_status ? 'selected' : '' }}>No</option>
-                                <option value="Yes" {{ 'Yes' === $requisition->coc_request_status ? 'selected' : '' }}>Yes</option>
+                                <option value="no" {{ 'no' === $requisition->coc_request_status ? 'selected' : '' }}>No</option>
+                                <option value="yes" {{ 'yes' === $requisition->coc_request_status ? 'selected' : '' }}>Yes</option>
                             </select>
                             <x-input-error :messages="$errors->get('coc_request_status')" class="mt-2" />
                         </div>
