19e1198cd0631297b4fc9dda8d4809a9fef93fcf
KrisNavela <krisnavela@gmail.com>
2024-11-25 15:11:39 +0800

    update controller 1

--- app/Http/Controllers/TreasuryApprovalController.php
+++ app/Http/Controllers/TreasuryApprovalController.php
@@ -105,17 +105,7 @@ class TreasuryApprovalController extends Controller
                     ->paginate(5)
                     ->withQueryString();
                 }
-            } else {
-                $requisitions = Requisition::withCount('items')
-                ->where('finalapproval_status', '=', 'approved')
-                ->where('treasuryapproval_status', '=', 'for approval')
-                ->orderBy('id', 'desc')
-                ->paginate(5)
-                ->withQueryString();    
             }
-
-            
-
 
             return view('treasuryapprovalrequisitions.index', [
                 'requisitions' => $requisitions,
