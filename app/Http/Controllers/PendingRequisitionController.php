2363bd27963fd7d379bba1b7550e82ce86333389
KrisNavela <krisnavela@gmail.com>
2024-11-28 16:29:51 +0800

    update code pending 1

--- app/Http/Controllers/PendingRequisitionController.php
+++ app/Http/Controllers/PendingRequisitionController.php
@@ -558,7 +558,7 @@ class PendingRequisitionController extends Controller
 
         $user = auth()->user(); // Get the authenticated user
         $userId = $user->id;
-        $rolename = $user->roles->name;
+        $rolename = $user->role->name;
 
         RequisitionRemarks::create([
             'requisition_id' => $pendingrequisition->id,
