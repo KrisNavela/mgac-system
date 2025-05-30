39c1d81dffb497af83da20675558cb9fa9763894
KrisNavela <krisnavela@gmail.com>
2025-05-30 11:45:01 +0800

    Update button 1

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -136,7 +136,7 @@
                         {{ __('Done') }}
                     </x-nav-link>
                 @endif
-                
+
                 </div>
             </div>
         </div>
@@ -157,10 +157,9 @@
             <div class="flex justify-end">
                 <a href="{{ route('requisitions.create') }}" class="bg-green-500 text-white hover:bg-green-700 text-sm px-4 py-2 rounded-md">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                    Create
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                    Add
                     </svg>
                 
                 </a>
