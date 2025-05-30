1841d6edde3e7d28c7a04deddf21a70e0cd1b45b
KrisNavela <krisnavela@gmail.com>
2025-05-30 11:27:58 +0800

    update 1

--- resources/views/requisitions/index.blade.php
+++ resources/views/requisitions/index.blade.php
@@ -153,7 +153,16 @@
         </form>
 
             <div class="flex justify-end">
-                <a href="{{ route('requisitions.create') }}" class="bg-blue-500 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">Create</a>
+                <a href="{{ route('requisitions.create') }}" class="bg-blue-500 text-white hover:bg-blue-700 text-sm px-2 py-1 rounded-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    Create
+                    </svg>
+                
+
+
+                </a>
             </div>
 
            
