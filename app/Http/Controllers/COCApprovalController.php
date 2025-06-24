9de4fd1ba2070453a41f30c1002c6763e451a602
KrisNavela <krisnavela@gmail.com>
2025-06-24 10:31:05 +0800

    add TMEC in COC approval

--- app/Http/Controllers/COCApprovalController.php
+++ app/Http/Controllers/COCApprovalController.php
@@ -130,13 +130,13 @@ class COCApprovalController extends Controller
 
             $requisitionsCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->count();
 
             $pendingrequisitionCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('status', '=', 'pending')
             ->where('finalapproval_status', '=', 'no')
@@ -145,7 +145,7 @@ class COCApprovalController extends Controller
 
             $uwapprovalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('status', '=', 'pending')
             ->where('uw_status', '=', 'for approval')
@@ -153,7 +153,7 @@ class COCApprovalController extends Controller
 
             $bondsapprovalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('status', '=', 'pending')
             ->where('bonds_status', '=', 'for approval')
@@ -161,7 +161,7 @@ class COCApprovalController extends Controller
 
             $collasstapprovalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'for approval')
@@ -171,7 +171,7 @@ class COCApprovalController extends Controller
 
             $collmngapprovalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('type_request', '=', 'replenishment')
             ->where('collasst_status', '=', 'approved')
@@ -181,28 +181,28 @@ class COCApprovalController extends Controller
 
             $cancelrequisitionsCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('status', '=', 'Cancelled')
             ->count();
 
             $approvedrequisitionsCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('finalapproval_status', '=', 'for approval')
             ->count();
 
             $fortransmittalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('status', '=', 'approved')
             ->count();
 
             $treasuryapprovalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('treasuryapproval_status', '=', 'for approval')
             ->where('finalapproval_status', '=', 'approved')
@@ -210,7 +210,7 @@ class COCApprovalController extends Controller
 
             $cocapprovalCount = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('cocapproval_status', '=', 'for approval')
             ->where('finalapproval_status', '=', 'approved')
@@ -218,7 +218,7 @@ class COCApprovalController extends Controller
 
             $requisitions = Requisition::whereHas('user', function ($query) {
                 $query->whereHas('branch', function ($query1) {
-                    $query1->where('type_office', 'Branch');}
+                    $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
             ->where('finalapproval_status', '=', 'approved')
             ->where('cocapproval_status', '=', 'for approval')
