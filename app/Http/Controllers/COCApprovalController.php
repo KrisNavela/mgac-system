7d6e6f1a62d304bdbce1e960dd0e33866bb33ad0
KrisNavela <krisnavela@gmail.com>
2025-07-03 11:32:00 +0800

    update code on coc approval

--- app/Http/Controllers/COCApprovalController.php
+++ app/Http/Controllers/COCApprovalController.php
@@ -220,7 +220,7 @@ class COCApprovalController extends Controller
                 $query->whereHas('branch', function ($query1) {
                     $query1->whereIn('type_office', ['Branch', 'TMEC']);}
             );})
-            ->where('finalapproval_status', '=', 'approved')
+            //->where('finalapproval_status', '=', 'approved')
             ->where('cocapproval_status', '=', 'for approval')
             ->orderBy('id', 'desc')
             ->paginate(10)
@@ -342,7 +342,7 @@ class COCApprovalController extends Controller
                 $query->whereHas('branch', function ($query1) {
                     $query1->where('type_office', 'Agency');}
             );})
-            ->where('finalapproval_status', '=', 'approved')
+            //->where('finalapproval_status', '=', 'approved')
             ->where('cocapproval_status', '=', 'for approval')
             ->orderBy('id', 'desc')
             ->paginate(10)
