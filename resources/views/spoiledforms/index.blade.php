6bf04b75b27a6e0c9c2c828c4dc65ba8e456d166
KrisNavela <krisnavela@gmail.com>
2025-07-29 13:19:59 +0800

    test update 1

--- resources/views/spoiledforms/index.blade.php
+++ resources/views/spoiledforms/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Spoiled Items') }}
+            {{ __('New Spoiled Items') }}
         </h2>
     </x-slot>
 
